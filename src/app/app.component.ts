46892da618dbd793df96447622376ff007d39601
Alcides Rodriguez <alcidesrodriguezreyes@gmail.com>
2018-11-12 14:59:06 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stock-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'stock-managment';
+}
